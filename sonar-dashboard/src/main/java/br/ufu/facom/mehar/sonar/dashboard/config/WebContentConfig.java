5e114762b4d2718c87a1d4a3c81d5b9bfcb38a5c
Mauricio Amaral Gonçalves <mauricioamaralg@gmail.com>
2019-02-15 15:58:03 -0200

    Experiments with Docker and SONAr Dashboard

---
+++ sonar-dashboard/src/main/java/br/ufu/facom/mehar/sonar/dashboard/config/WebContentConfig.java
@@ -0,0 +1,12 @@
+package br.ufu.facom.mehar.sonar.dashboard.config;
+
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//public class WebContentConfig extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("forwar:/index.html");
+//    }
+//}
