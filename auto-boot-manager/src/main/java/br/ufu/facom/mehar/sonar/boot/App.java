28cf632ad208e5430980dc19fdcd4d92ce165cb4
Mauricio Amaral Gonçalves <mauricioamaralg@gmail.com>
2019-03-20 10:57:46 -0300

    Implementation of auto-boot-manager and dhcp support. Pt. 1 - First experiments

--- auto-boot-manager/src/main/java/br/ufu/facom/mehar/sonar/boot/App.java
+++ auto-boot-manager/src/main/java/br/ufu/facom/mehar/sonar/boot/App.java
@@ -1,13 +1,26 @@
 package br.ufu.facom.mehar.sonar.boot;
 
+import java.io.IOException;
+
+import org.apache.directory.server.dhcp.netty.DhcpServer;
+import org.apache.directory.server.dhcp.service.manager.LeaseManager;
+import org.apache.directory.server.dhcp.service.store.FixedStoreLeaseManager;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		LeaseManager leaseManager = new FixedStoreLeaseManager();
+		DhcpServer server = new DhcpServer(leaseManager);
+		
+		try {
+			server.addDefaultInterfaces();
+			
+//			server.
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
